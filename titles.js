8bbbea85c7155b47de3ad498ff5c76db1cc42e28
ladynobeard <syh031@ucsd.edu>
2018-09-27 20:42:21 -0700

    Add files via upload
    finished mk 4

---
+++ titles.js
@@ -0,0 +1,73 @@
+var sea = {
+	label: ["sea", "ocean", "beach", "waves", "wave", "beaches", "oceans", "shores", "shore"],
+	links: ["A2_yg19Pu7Y","xGRjCa49C6U","qREKP9oijWI","lxNXf9fBv0g","jKCyFB5LmPo", "mMhd7UZY9qw", "qPTJIqGHOXM","RNwQn5T0wRs"],
+};
+
+var coral = {
+	label: ["coral","corals"],
+	links: ["_05mQPHk46Q"]
+};
+
+var mountain = {
+	label: ["mountain", "mountains"],
+	links: ["ybhZy3vGeUM","0fHaQdacCmM"],
+};
+
+var river = {
+	label: ["river", "stream", "rivers", "streams"],
+	links: ["1t7g690boao","IadsLclBOS8","Ftm2uv7-Ybw", "czCHvDY23fc", "6uhRxK_EOm4","0EY28tdiFlc"],
+};
+
+var snow = {
+	label: ["snow", "winter"],
+	links: ["0EY28tdiFlc","0fHaQdacCmM","CUaybv1jdHw"],
+};
+
+var waterfall = {
+	label: ["waterfall", "waterfalls"],
+	links: ["JdlTsEoJtLA",],
+};
+
+var night = {
+	label: ["night","nighttime"],
+	links: ["9xyUEVeJPO8",],
+};
+
+var jungle = {
+	label: ["jungle", "jungles"],
+	links: ["9xyUEVeJPO8","JdlTsEoJtLA"],
+};
+
+var forrest = {
+	label: ["forrest", "forrests"],
+	links: ["IadsLclBOS8","XxP8kxUn5bc"],
+};
+
+var grass = {
+	label: ["grass", "plains", "grassland", "meadowland", "meadows", "meadow", "pasture", "pastures"],
+	links: ["3HcWa7lbv9Y", "1D3dOrba8f8"],
+};
+
+var nyc ={
+	label: ["new york", "new york city"],
+	links: ["la90mA4VLa4"]
+};
+
+var earth ={
+	label: ["earth"],
+	links: ["lUQt2iLKqBbI"]
+};
+
+var fuji ={
+	label:["mount fuji", "fuji"],
+	links: ["iyzGqj_xRfc"],
+};
+
+var videos = [fuji, nyc, snow, earth, coral, sea, mountain, river, night, jungle, forrest, grass];
+
+var printTitle = function(title){
+	console.log(title.label);
+}
+
+
+
